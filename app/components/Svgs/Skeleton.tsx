293eb19b2a92fe89a9d4e04f9ac10c693cfc0acb
02Alexis <m_chael1012@hotmail.com>
2023-09-08 15:34:05 -0500

    'npm run build'

--- app/components/Svgs/Skeleton.tsx
+++
@@ -1,9 +0,0 @@
-import ContentLoader from "react-content-loader";
-
-const Skeleton = () => {
-  return (
-    <div className="w-[250px] h-[250px] sm:h-[350px] sm:w-[350px] md:h-[400px] md:w-[400px] lg:h-[500px] lg:w-[500px] flex items-center justify-center m-0 bg-light dark:bg-dark" />
-  );
-};
-
-export default Skeleton;
